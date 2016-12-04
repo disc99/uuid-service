194c692e6a0ef7bdc6e1961607362968385e1a82
d.kanehira <disc99.g@gmail.com>
2016-12-05 00:49:28 +0900

    Add pool logic

--- main.go
+++ main.go
@@ -11,17 +11,62 @@ import (
 	"google.golang.org/grpc"
 )
 
-type Service struct{}
+type Service struct {
+	pool map[string]map[string]string
+}
 
-func (*Service) Generate(ctx context.Context, req *pb.UuidRequest) (*pb.UuidResponse, error) {
-	out, err := exec.Command("uuidgen").Output()
+func (service *Service) Generate(ctx context.Context, req *pb.UuidRequest) (*pb.UuidResponse, error) {
+
+	// Generate UUID
+	uuid, err := Uuid()
 	if err != nil {
 		return nil, err
 	}
+	log.Println("Generate UUID: " + uuid)
+
+	// TODO Pool check
+	ids, existKey := service.pool[req.Key]
+	if !existKey {
+		if ids == nil {
+			ids = map[string]string{}
+		}
+		_, exist := ids[uuid]
+		if !exist {
+			ids[uuid] = ""
+			return &pb.UuidResponse{Uuid: uuid}, nil
+		}
+	}
+
+	// Re Generate UUID
+	uuid, err = Uuid()
+	if err != nil {
+		return nil, err
+	}
+	log.Println("Re Generate UUID: " + uuid)
+
+	// TODO Re pool check
+	ids, existKey = service.pool[req.Key]
+	if !existKey {
+		if ids == nil {
+			ids = map[string]string{}
+		}
+		_, exist := ids[uuid]
+		if !exist {
+			ids[uuid] = ""
+			return &pb.UuidResponse{Uuid: uuid}, nil
+		}
+	}
+	return nil, err
+}
+
+func Uuid() (string, error) {
+	out, err := exec.Command("uuidgen").Output()
+	if err != nil {
+		return "", err
+	}
 	uuid := string(out[:])
 	uuid = strings.TrimRight(uuid, "\n")
-	log.Println("Generate UUID: " + uuid)
-	return &pb.UuidResponse{Uuid: uuid}, nil
+	return uuid, nil
 }
 
 func main() {
@@ -32,6 +77,8 @@ func main() {
 		log.Fatalln(err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterUuidGeneratorServer(s, &Service{})
+	pb.RegisterUuidGeneratorServer(s, &Service{
+		pool: map[string]map[string]string{},
+	})
 	s.Serve(l)
 }
